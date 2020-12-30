90005238833f492d6ea70eff78fd270e8eeb9008
Jejune <dalitti@waschbaerbau.ch>
2020-12-30 12:35:33 +0100

    Updated for udacity submission
    changed type definition

--- image-filter-starter-code/src/server.ts
+++ image-filter-starter-code/src/server.ts
@@ -13,33 +13,16 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   // Use the body parser middleware for post requests
   app.use(bodyParser.json());
 
-  // @TODO1 IMPLEMENT A RESTFUL ENDPOINT
-  // GET /filteredimage?image_url={{URL}}
-  // endpoint to filter an image from a public url.
-  // IT SHOULD
-  //    1
-  //    1. validate the image_url query
-  //    2. call filterImageFromURL(image_url) to filter the image
-  //    3. send the resulting file in the response
-  //    4. deletes any files on the server on finish of the response
-  // QUERY PARAMATERS
-  //    image_url: URL of a publicly accessible image
-  // RETURNS
-  //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
-
-  /**************************************************************************** */
-
-  //! END @TODO1
   // testImage: https://www.litter.me/wp/wp-content/uploads/2020/09/DJI_0094-scaled.jpg
-  app.get("/FilteredImage", async (req, res) => {
-      const sourceImageUrl = req.query.imageUrl;
+  app.get("/FilteredImage", async (req:express.Request, res:express.Response) => {
+      const sourceImageUrl:string = req.query.imageUrl;
 
       if (!sourceImageUrl) {
         res.status(404).send({status: "Error",message: "Please provide valid imnageUrl."});
         return;
       }
       
-      var filteredImage = await filterImageFromURL(sourceImageUrl);
+      const filteredImage:string = await filterImageFromURL(sourceImageUrl);
 
       if (!filteredImage) {
         res.status(500).send({status: "Error",message: "Image conversion failed."});
